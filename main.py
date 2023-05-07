595bd997bf5351e9c35401a1c79826c10b943c5e
jewook <gjwpdnr0918@gmail.com>
2023-05-07 12:46:27 +0900

    df

--- main.py
+++ main.py
@@ -1,46 +1,16 @@
-import os
-import json
-import google_auth_oauthlib.flow
-from googleapiclient.discovery import build
-import googleapiclient.errors
-import pandas as pd
-import numpy as np
-from google.oauth2 import service_account
-import kbs.kbs_info as KBS
-from kbs.data_updater import update
+# This is a sample Python script.
 
-from kbs.data_fetcher import *
-
-columns = [
-    'videoId',
-    'publishedAt',
-    'title',
-    'description',
-    'categoryId',
-    'duration',
-    'dimension',
-    'definition',
-    'caption',
-    'contentRating',
-    'viewCount',
-    'likeCount',
-    'commentCount'
-]
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-if __name__ == "__main__":
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
-    credentials = service_account.Credentials.from_service_account_file(
-        os.path.join('info', 'service_account_key.json'), scopes=KBS.scopes)
-    # Run the OAuth flow to obtain credentials
-    youtube = build(KBS.API_SERVICE_NAME, KBS.API_VERSION, credentials=credentials)
-    try:
-        df = pd.read_csv(KBS.DATA_PATH)
-    except:
-        df = pd.DataFrame(columns=KBS.COLUMNS)
 
-    print(df.columns)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-    update(youtube, df)
-    print(df.head())
-    df.to_csv(KBS.DATA_PATH, index=False)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
